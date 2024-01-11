a78581c5c8d2e5f3234026391d4216281e3b807f
Vasco21 <vascoeti123@gmail.com>
2024-01-12 01:44:39 +0200

    update

--- src/Components/VipTable.js
+++ src/Components/VipTable.js
@@ -4,14 +4,16 @@ import logo from "../images/logo.png"
 
 const VipTable = () => {
   const data = [
-    { level: 'Experience Level', depositAmt: 'R300', dailyIncome: ' 6 - 8', monthlyIncome: '490 - 550' },
-    { level: 'VIP 1', depositAmt: 'R800', dailyIncome: '22 - 28', monthlyIncome: '1400 - 1600' },
-    { level: 'VIP 2', depositAmt: 'R2000', dailyIncome: '63 - 70', monthlyIncome: '4200 - 4500' },
-    { level: 'VIP 3', depositAmt: 'R5000', dailyIncome: '150 - 190', monthlyIncome: '12000 - 19000' },
-    { level: 'VIP 4', depositAmt: 'R120000', dailyIncome: '760 - 900', monthlyIncome: '63000 - 68000' },
-    { level: 'VIP 5', depositAmt: 'R120000', dailyIncome: '2400 - 2800', monthlyIncome: '220000 - 270000' },
-    { level: 'VIP 6', depositAmt: 'R200000', dailyIncome: '14000 - 17000', monthlyIncome: '800K - 1M' },
-    { level: 'VIP 7', depositAmt: 'R400000', dailyIncome: '34000 - 40000', monthlyIncome: '8M - 9M' },
+    { level: 'Experience Level', depositAmt: '--', dailyIncome: ' 6 - 8', monthlyIncome: '7Days Trail' },
+    { level: 'VIP 1', depositAmt: 'R300', dailyIncome: '6 - 8', monthlyIncome: '400 - 550' },
+    { level: 'VIP 2', depositAmt: 'R800', dailyIncome: '22 - 28', monthlyIncome: '1400 - 1600' },
+    { level: 'VIP 3', depositAmt: 'R2000', dailyIncome: '63 - 70', monthlyIncome: '4200 - 4500' },
+    { level: 'VIP 4', depositAmt: 'R5000', dailyIncome: '150 - 190', monthlyIncome: '12000 - 19000'},
+    { level: 'VIP 5', depositAmt: 'R20000', dailyIncome: '760 - 900', monthlyIncome: '63000 - 68000' },
+    { level: 'VIP 6', depositAmt: 'R50000', dailyIncome: '2400 - 2800', monthlyIncome: '220000 - 270000' },
+    { level: 'VIP 7', depositAmt: 'R120000', dailyIncome: '7500 - 8500', monthlyIncome: '800K - 1M' },
+    { level: 'VIP 8', depositAmt: 'R200000', dailyIncome: '14000 - 17000', monthlyIncome: '2M - 2.5M' },
+    { level: 'VIP 9', depositAmt: 'R400000', dailyIncome: '34000 - 40000', monthlyIncome: '8M - 9M' },
   ];
 
   return (
